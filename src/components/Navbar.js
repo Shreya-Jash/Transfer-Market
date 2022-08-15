9a88ac1f5eeec3cd18ff865cd1c1bb86f1263b9e
Shreya Jash <shreyajash98@gmail.com>
2022-08-15 20:46:21 +0530

    css-update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {NavLink} from "react-router-dom";
 import "../components/Navbar.css"
 
 function Navbar() {
+    const [isMobile,setIsMobile] = React.useState(false);
   return (
     <nav>
-        <ul>
-            <NavLink to="/">
-                <li>Home</li>
+        <div className='header'>
+            <NavLink to="/"  className="logo">
+                Logo
             </NavLink>
+            <button
+                className='mobile-responsive'
+                onClick={() => setIsMobile(!isMobile)}
+            >
+                {isMobile ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
+            </button>
+        </div>
+        <ul 
+            className={isMobile ? "nav-links-mobile" : "navList"}
+            onClick={() => setIsMobile(false)}
+        >
             <NavLink to="/">
                 <li>News</li>
             </NavLink>
@@ -18,9 +30,10 @@ function Navbar() {
             <NavLink to="/">
                 <li>Topic 3</li>
             </NavLink>
-            <NavLink to="/">
+            <NavLink to="/" className="topic4">
                 <li>Topic 4</li>
             </NavLink>
+            
         </ul>
         
     </nav>
